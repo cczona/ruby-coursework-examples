d2fdd5ee57745b5ae454970744bab7e539a32dad
cczona <cczona@gmail.com>
2011-10-15 21:29:18 -0700

    ADD code provided in instructor examples

--- lab5/cgi_helper.rb
+++ lab5/cgi_helper.rb
@@ -6,6 +6,43 @@ module CGI_Helper
   end
 end
 
+## instructor-provided example from ERB reading
+# render_erb creates an ERB object using a string
+# render_file_as_erb opens a file and render it as ERB, using render_erb
+
+module CgiHelper
+
+ def render_file_with_erb(file)
+   rhtml = File.read file
+     render_erb(rhtml)
+  end
+
+  def render_erb(rhtml)
+   require 'erb'
+   erb = ERB.new(rhtml)
+   erb.result(binding)
+ end
+
+end
+
+## Assuming we have a file named index.html, we can render it as HTML using ERB.
+#
+# <!-- index.html --> <%= @name %>
+# We can use this module like this:
+#
+# #!/usr/local/bin/ruby
+# require 'cgi_helper'
+# include CgiHelper
+# http_header
+# @name = 'Yosemite Sam'
+# puts render_file_with_erb('index.rhtml')
+# And the output will be:
+#
+# <!-- index.html -->
+# Yosemite Sam
+##
+
+
 class String
-  include CGI_Helper 
+  include CGI_Helper
 end
