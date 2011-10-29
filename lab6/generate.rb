2f38023c5f4476dde9bd7cebdb7e64322997a976
cczona <cczona@gmail.com>
2011-10-29 12:19:40 -0700

    ADD initial tests for generate.rb & code that passes them

---
+++ lab6/generate.rb
@@ -0,0 +1,16 @@
+class Generate
+  
+  def is_production?
+    #  another candidate: ENV['SERVER_SIGNATURE'].match(%r{hills.ccsf.cc.ca.us})
+    ( ENV["HTTP_HOST"]=="hills.ccsf.edu" || ENV["SERVER_ADDR"]=='147.144.1.2' || ENV['USER'] == 'czona' )
+  end
+  
+  def path
+    if is_production? #FIXME: unsandbox these!
+      @path=File.expand_path('/tmp/czona/')
+    else
+      @path=File.expand_path('/tmp/lab6/')
+    end
+  end
+  
+end
