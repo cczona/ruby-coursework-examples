4720fa1a693023032aa8d68d344cf3198665c8f2
cczona <cczona@gmail.com>
2011-10-30 16:07:07 -0700

    ADD a bunch of tests w/passing code per req'ts

--- lab6/generate.rb
+++ lab6/generate.rb
@@ -1,18 +1,53 @@
 #!/usr/bin/env ruby
 
 class Generate
+  ALLOWED_CHARACTERS=('a'..'z').to_a + ('A'..'Z').to_a  + ('0'..'9').to_a  << '-' << '_'
+
+  def initialize
+    $:.unshift script_directory # add app base directory to load path
+  end
 
   def is_production?
     #  another candidate: ENV['SERVER_SIGNATURE'].match(%r{hills.ccsf.cc.ca.us})
     ( ENV["HTTP_HOST"]=="hills.ccsf.edu" || ENV["SERVER_ADDR"]=='147.144.1.2' || ENV['USER'] == 'czona' )
   end
-  def path
 
+  def script_directory
     if is_production? #FIXME: unsandbox these!
-      @path=File.expand_path('/tmp/czona/')
+      File.expand_path('/tmp/czona/')
     else
-      @path=File.expand_path('/tmp/lab6/')
+      File.expand_path('/tmp/lab6/')
     end
   end
 
+  def valid_input?
+    unless ARGV.class == Array        ; raise Exception, "That's weird.  ARGV isn't an array for some reason.";end
+    if (ARGV.length < 1)              ; raise ArgumentError, 'Must provide one argument';end
+    if (ARGV.length > 1)              ; raise ArgumentError, "Just one argument, please";end
+    unless (ARGV[0].class == String)  ; raise ArgumentError, "Sorry, the argument must be a string";end
+    unless (sane?)                    ; raise ArgumentError, "Sorry, the name '#{ARGV[0]}' is not allowed. Stick to strings consisting of ASCII letters, numbers, underscores, and hyphens.";end
+    return true
+  end
+
+  def sane?
+    letters=ARGV[0].split('').uniq # beware: uniq! returns nil when no changes are made
+    return false if letters.size == 0
+    passable=ALLOWED_CHARACTERS
+    letters.each { |ltr| return false unless passable.include? ltr }
+    return true
+  end
+
+  def app_name
+    if valid_input?
+      ARGV[0].to_s
+    else
+      raise RuntimeError "Invalid input somehow was introduced"
+    end
+  end
+
+  def new_app_directory
+    File.expand_path('./' + app_name, script_directory)
+  end
+
 end
+
--- lab6/specs/generate-test.rb
+++ lab6/specs/generate-test.rb
@@ -11,16 +11,88 @@ describe "Generate" do
     @tested=Generate.new
   end
 
+  it 'is named generate.rb' do
+    $".include? File.expand_path("../generate.rb")
+  end
+
   it 'is tested in the development environment' do
     @tested.is_production?.must_equal false
   end
 
-  it 'is configured to work under the system tmp heirarchy' do
-    @tested.base.must_match /^\/tmp\//
+  it 'is temporarily configured to work under the system tmp heirarchy' do
+    @tested.script_directory.must_match /^\/tmp\//
   end
 
-  it 'has a path config that matches lab6_files path config' do
-    @tested.path.must_equal Lab6Files.new.path
+  it 'does its work in the same directory that lab6_files reads from' do
+    ARGV<< 'foo'
+    @tested.script_directory.must_equal Lab6Files.new.path
+  end
+
+  it 'sets the load path to include the app base directory' do
+    $:.must_include @tested.script_directory
+  end
+
+  it 'this argument will be used to name a directory' do
+    ARGV.unshift "foo"
+    @tested.app_name.must_equal 'foo'
+    @tested.app_name.must_equal ARGV[0]
+    File.basename(@tested.new_app_directory).must_equal @tested.app_name
+  end
+
+  # it 'creates a directory structure populated with directories and files' do
+  #   string=''
+  #   chars=@tested.ALLOWED_CHARACTERS
+  #   8.times { string += chars.sample }
+  #   ARGV.unshift(string)
+  #   @tested.create
+  #   Dir.entries.must_include @tested.app_name
+  # end
+
+  it 'can sanitize a string by removing characters that are not acceptable' do
+    # fail
+    ARGV.clear.unshift'FOOOOOOOOOOOO!'
+    @tested.sane?.must_equal false
+    ARGV.clear.unshift '1234#'
+    @tested.sane?.must_equal false
+    ARGV.clear.unshift '/@'
+    @tested.sane?.must_equal false
+    ARGV.clear.unshift ''
+    @tested.sane?.must_equal false
+
+    #pass
+    ARGV.clear.unshift '1'
+    @tested.sane?.must_equal true
+    ARGV.clear.unshift '_'
+    @tested.sane?.must_equal true
+  end
+
+end
+
+
+# "The user will provide one argument to the script, which will be available in $ARGV"
+#  "accepts commandline arguments via ARGV"
+describe "Generate accepts one valid argument via ARGV" do
+
+  before do
+    ARGV.clear
+  end
+
+  it 'throws error when ARGV has too FEW arguments' do
+    ARGV.length.must_equal 0
+    proc {Generate.new.valid_input?}.must_raise ArgumentError
+  end
+
+  it 'throws error when ARGV has too MANY arguments' do
+    ARGV << :a
+    ARGV << :b
+    ARGV.length.must_equal 2
+    proc {Generate.new.valid_input?}.must_raise ArgumentError
+  end
+
+  # workaround since MiniTest does not permit negative test for exception
+  it 'passes validation when ARGV has a single string value' do
+    ARGV << 'a'
+    Generate.new.valid_input?.must_equal true
   end
 
 end
