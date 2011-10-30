12840a0928df252f2a9cee87b1e566b86e152ab2
cczona <cczona@gmail.com>
2011-10-30 12:07:07 -0700

    trivial: whitespace fixes

--- lab6/specs/generate-test.rb
+++ lab6/specs/generate-test.rb
@@ -3,7 +3,9 @@ require_relative '../generate.rb'
 # load, because 1.9.1's require/require_relative balk when there's a file extension they're not expecting
 load(File.dirname(Dir.pwd) + '/lab6_files.cgi')
 
+
 describe "Generate" do
+
   before do
     @tested=Generate.new
   end
@@ -13,10 +15,11 @@ describe "Generate" do
   end
 
   it 'is configured to work under the system tmp heirarchy' do
-    @tested.path.must_match /^\/tmp\//
+    @tested.base.must_match /^\/tmp\//
   end
-  
+
   it 'has a path config that matches lab6_files path config' do
     @tested.path.must_equal Lab6Files.new.path
   end
+
 end
